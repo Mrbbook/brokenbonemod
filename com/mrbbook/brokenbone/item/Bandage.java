5b7e04b789745a31471d964d7263fd1bf7255192
Tanawat Boonmak <latte.book@gmail.com>
2016-05-12 10:19:47 +0700

    Create Bandage.java

---
+++ com/mrbbook/brokenbone/item/Bandage.java
@@ -0,0 +1,20 @@
+package com.mrbbook.brokenbone.item;
+
+
+public class Bandage extends Item {
+
+
+  public itemBandage(String unlocalizedName)
+  {
+    setMaxStackSize(15);
+    setUnlocalizedName(unlocalizedName);
+    setTextureName("brokenbone:" + "/bandage/unlocalizedName");
+
+  }
+
+
+
+
+
+
+}
