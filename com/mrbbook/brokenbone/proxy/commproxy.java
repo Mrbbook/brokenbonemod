d72223e83ec687e1bf72eb752c75d756956edf96
Tanawat Boonmak <latte.book@gmail.com>
2016-05-12 09:59:20 +0700

    Create commproxy.java

---
+++ com/mrbbook/brokenbone/proxy/commproxy.java
@@ -0,0 +1,17 @@
+package com.mrbbook.brokenbone.proxy;
+
+import cpw.mods.fml.client.registry.RenderingRegistry;
+import cpw.mods.fml.common.registry.LanguageRegistry;
+import net.minecraftforge.common.MinecraftForge;
+
+public class commproxy  {
+
+
+public void registerRenderInformation(){
+
+//render code from client proxy go here
+
+}
+
+
+}
