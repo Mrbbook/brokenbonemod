dc3f72972d673c4f365dc620a9fd69f5d846d4ef
Tanawat Boonmak <latte.book@gmail.com>
2016-05-12 09:46:37 +0700

    Create Brokenbone.java

---
+++ com/mrbbook/brokenbone/Brokenbone.java
@@ -0,0 +1,23 @@
+package com.mrbbook.brokenbone;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.Mod.Instance;
+import cpw.mods.fml.common.SidedProxy;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.eventhandler.EventBus;
+import cpw.mods.fml.common.network.NetworkRegistry;
+import cpw.mods.fml.common.network.simpleimpl.SimpleNetworkWrapper;
+import cpw.mods.fml.common.registry.GameRegistry;
+import cpw.mods.fml.relauncher.Side;
+import minezmod.item.itembandage;
+import net.minecraft.item.Item;
+import net.minecraftforge.common.MinecraftForge;
+
+@Mod(modid="brokenbone", name="Brokenbone", version="0.1")
+public class Brokenbone {
+
+
+
+}
